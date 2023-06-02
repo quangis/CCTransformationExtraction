dd9641ac7c6ef5718f3210c07c6e200bae310bec
Xu <h.xu1@uu.nl>
2023-06-02 08:38:05 +0200

    Grammar code complete update

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
+import json
+import re
 from spacy.lang.en import English
 import en_core_web_sm
-import json
 from word2number import w2n
-from ner import core_concept_match
+from ner import core_concept_match  # only include core concept recognition, place name and entity are recognized by blockly interface
 from geoparser import geo_parser
 from transformations import write_trans
 
 results = []
-questionFilepath = 'corpus.txt'
+# BlocklyJson = "test.json"
+# BlocklyJson = "blocklyoutput.json"
+BlocklyJson = "blocklyoutput_GeoAnQu.json"
 sym = '" ? \n \t'  # remove these symbols from questions
-
-errorQuestionFilePath = 'error.txt'
-error_ques = open(errorQuestionFilePath, 'w+')
 
 
 # [X]  customized a list of stopwords
 class CustomEnglishDefaults(English.Defaults):
     # stop_words = set(["is", "are", "was", "were", "do", "does", "did", "have", "had"])
-    stop_words = {"do", "did", "does", "a", "an", "the", "their", 'his', 'her', 'my'}
+    stop_words = {'do', 'did', 'does', 'a', 'an', 'the', 'their', 'his', 'her', 'my', 'your'}
 
 
 class CustomEnglish(English):
@@ -58,48 +58,81 @@ def word2num(sentence):
 
     return sentence
 
-
-def readfile(questionFilepath):
-    with open(questionFilepath, encoding="utf-8") as questions:
-        for question in questions:
-            result = {}
+# [X]read Json input
+def readJson(BlocklyJson):
+    with open(BlocklyJson, 'r') as f:
+        jsonBlocks = json.load(f)
+        # instantiate a class
+        # parser = QuestionParser(None)
+        # cctAnnotator = TypesToQueryConverter()
+        for result in jsonBlocks:
+            # request to parse the result
+            # result = parser.parseQuestion(resultparam) # resultparm is each json block
+            # cctAnnotator.algebraToQuery(result, True, True)
+            # cctAnnotator.algebraToExpandedQuery(result, False, False)
+            coreConTrans = {}
+            coreTypeDict = {}
             core_id = 0
-            coreConTrans = {}  # final cctrans output
-            result['question'] = question.strip(sym)
-            doc = nlp_en(result['question'])
+            result['question'] = ' '.join(result['question'].split())  # remove extra whitespaces in string
+            result['replaceQ'] = ' '.join(result['replaceQ'].split()).replace('-', ' ').replace('?', '')
+
+            # print('*********************************')
+            # print('question\n', result['question'])
+
+            # [X] loop every key in result and replace keywords with its tag
+            for key, value in result.items():
+                if (key != 'question' or key != 'replaceQ') and isinstance(value, list) and value:
+                    for i, item in enumerate(value):
+                        if item == 'in':
+                            rq_list = result['replaceQ'].split()
+                            rq_list[rq_list.index('in')] = key + str(i)
+                            result['replaceQ'] = ' '.join(rq_list)
+                        else:
+                            result['replaceQ'] = result['replaceQ'].replace(item, f"{key}{i}")
+
+            # [X] print entities and placenames
+            entity = {}
+            for key, value in result.items():
+                if (key != 'question' or key != 'replaceQ') and isinstance(value, list) and value:
+                    entity[key] = result[key]
+            # print('entity\n', entity)
+
 
             # [X] Cleaning text: remove stopwords and save the tokens in a list
-            # text = ' '.join([word for word in text if word not in string.punctuation])
+            doc = nlp_en(result['replaceQ'])
             token_list = []
             for word in doc:
                 if not word.is_stop and not word.is_punct or word.text == ',':
                     token_list.append(word)
             sen = ' '.join(word.text for word in token_list).strip()  # Question in string without stopwords
             sen_Clean = word2num(sen)  # Convert numeric words into digit numbers
-            result['cleaned_Question'] = sen_Clean
+            result['replaceQ'] = re.sub('\s+', ' ', sen_Clean).strip()
+            # print('replaceQ\n', result['replaceQ'])
 
-            # [X] Identify Placename, Entity, Core Concepts
-            re_CoreCon = core_concept_match(sen_Clean)  # parsed_Entities[1]: sentence
+            # [X] Identify Core Concepts
+            re_CoreCon = core_concept_match(result['replaceQ'].lower())  # parsed_Entities[1]: sentence
             result.update(re_CoreCon[0])  # re_CoreCon[0]: dictionary - Core Concepts
             result['ner_Question'] = re_CoreCon[1]
+            # print('ner_Question\n', result['ner_Question'])
+            print('result\n', result)
 
-            #[X] Generate parser tree
-            parsedQuestion = geo_parser(result, core_id, coreConTrans)
+            # [X] Generate parser tree
+            parsedQuestion = geo_parser(result, core_id, coreTypeDict, coreConTrans)
             if parsedQuestion[0]:
                 result['parseTreeStr'] = parsedQuestion[0]
-            if parsedQuestion[1] and coreConTrans:
+            if coreTypeDict and coreConTrans:
                 result['cctrans'] = write_trans(parsedQuestion)
-            if parsedQuestion[4]:
-                error_ques.write(parsedQuestion[3] + '\n')  # questions can not be parsed in the grammar
+            # print(parsedQuestion)
 
-            print(parsedQuestion)
             results.append(result)
+            # print(results)
 
-    with open('parse results.json', 'w') as outputFile:
+    # with open('parse_results_retri_0601.json', 'w') as outputFile:
+    #     json.dump(results, outputFile)
+
+    with open('GeoAnQu_parser_results_0601.json', 'w') as outputFile:
         json.dump(results, outputFile)
-    error_ques.close()
-
 
 
 if __name__ == '__main__':
-    readfile(questionFilepath)
+    readJson(BlocklyJson)
